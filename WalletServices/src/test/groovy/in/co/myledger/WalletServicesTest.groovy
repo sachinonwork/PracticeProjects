d324c716ece4b2b13575c75e9d3cf5c070da47e9
sachin.danayak@gmail.com <sachin.danayak@gmail.com>
2016-11-05 11:19:25 +0530

    Failing test for the service created

--- WalletServices/src/test/groovy/in/co/myledger/WalletServicesTest.groovy
+++ WalletServices/src/test/groovy/in/co/myledger/WalletServicesTest.groovy
@@ -1,9 +1,27 @@
 package in.co.myledger
 
+import org.junit.Test
+import spock.lang.Specification
+import groovyx.net.http.RESTClient
 
-class WalletServicesTest {
 
-    void "Create a transaction and check if service give 201 response"() {
+class WalletServicesTest extends Specification {
 
+    def localServer = "http://localhost:8080"
+    def restClient = new RESTClient(localServer);
+
+    @Test
+    def "Create a transaction and check if service give 201 response"() {
+        setup:
+        given: "Valid request for /wallet/transaction"
+
+        when: "request to create a transaction"
+        def response = restClient.post([path:'/create', query:[amount:10, currency:'INR', expenseDesc:'snacks in evening' ]])
+
+        then: "create transaction gives success and 201 response"
+        with(response) {
+            status == 201
+            contentType == "application/json"
+        }
     }
 }
