735afa5c9d6ba0d3e5a3ff9f7e7ef6a60915b557
Sachin Danayak <sachin.danayak@gmail.com>
2017-03-20 23:04:38 +0530

    Added swagger config

--- SwaggerSample/src/main/java/in/co/sample/config/SwaggerConfig.java
+++ SwaggerSample/src/main/java/in/co/sample/config/SwaggerConfig.java
@@ -1,30 +1,40 @@
 package in.co.sample.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@EnableAutoConfiguration
 public class SwaggerConfig {
 
-
+/*
     private SwaggerConfig springSwaggerConfig;
 
     @Autowired
     public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
         this.springSwaggerConfig = springSwaggerConfig;
-    }
+    }*/
 
-    @Bean
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
+
+    /*@Bean
     public SwaggerSpringMvcPlugin customImplementation() {
         return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
                 //This info will be used in Swagger. See realisation of ApiInfo for more details.
                 .apiInfo(new ApiInfo(
-                        "SmartMe education API",
-                        "This app is for education, training purpose. It represents model of landlords and apartments for rent",
+                        "My Messages API",
+                        "Sample app for messages",
                         null,
                         null,
                         null,
@@ -33,7 +43,7 @@ public class SwaggerConfig {
                 //Here we disable auto generating of responses for REST-endpoints
                 .useDefaultResponseMessages(false)
                 //Here we specify URI patterns which will be included in Swagger docs. Use regex for this purpose.
-                .includePatterns("/landlords.*");
-    }
+                .includePatterns("/mymessages.*");
+    }*/
 
 }
--- SwaggerSample/src/main/java/in/co/sample/controllers/WelcomeController.java
+++ SwaggerSample/src/main/java/in/co/sample/controllers/WelcomeController.java
@@ -6,15 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
-
 @Api(basePath = "/mymessages", value = "My Messages", description = "Messages for the user", produces = "application/json")
 @RestController
 
 public class WelcomeController {
 
     @RequestMapping(value = "/mymessages/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String showWelcomeMessage(@PathParam("name") String name) {
+    public String showWelcomeMessage(@RequestParam("name") String name) {
         return "Welcome " + name;
     }
 }
