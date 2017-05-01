20c02440f0269602af1f2f1e110e84b0f72aa297
Sachin Danayak <sachin.danayak@gmail.com>
2017-05-01 12:47:44 +0530

    Removed unwanted folders and worked sample with swagger

--- SwaggerSample/src/main/java/in/co/sample/config/SwaggerConfig.java
+++ SwaggerSample/src/main/java/in/co/sample/config/SwaggerConfig.java
@@ -7,17 +7,11 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
-
-/*
-    private SwaggerConfig springSwaggerConfig;
-
-    @Autowired
-    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
-        this.springSwaggerConfig = springSwaggerConfig;
-    }*/
 
         @Bean
         public Docket api() {
@@ -27,23 +21,4 @@ public class SwaggerConfig {
                     .paths(PathSelectors.any())
                     .build();
         }
-
-    /*@Bean
-    public SwaggerSpringMvcPlugin customImplementation() {
-        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
-                //This info will be used in Swagger. See realisation of ApiInfo for more details.
-                .apiInfo(new ApiInfo(
-                        "My Messages API",
-                        "Sample app for messages",
-                        null,
-                        null,
-                        null,
-                        null
-                ))
-                //Here we disable auto generating of responses for REST-endpoints
-                .useDefaultResponseMessages(false)
-                //Here we specify URI patterns which will be included in Swagger docs. Use regex for this purpose.
-                .includePatterns("/mymessages.*");
-    }*/
-
 }
--- SwaggerSample/src/main/java/in/co/sample/controllers/WelcomeController.java
+++ SwaggerSample/src/main/java/in/co/sample/controllers/WelcomeController.java
@@ -1,18 +1,25 @@
 package in.co.sample.controllers;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@Api(basePath = "/mymessages", value = "My Messages", description = "Messages for the user", produces = "application/json")
+
+@Api(basePath = "/mymessages", value = "My Messages", description = "Messages for the user", consumes = "application/json",
+        produces = "application/json")
 @RestController
-
 public class WelcomeController {
 
-    @RequestMapping(value = "/mymessages/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String showWelcomeMessage(@RequestParam("name") String name) {
+    @RequestMapping(value = "/mymessages/{name}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String showWelcomeMessage(@PathVariable("name") String name) {
         return "Welcome " + name;
+    }
+
+
+    @ApiOperation(value="For adding two number returns string of addition result", response = String.class)
+    @RequestMapping(value = "/mymessages/sayNumber", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String showRandomNumber(@RequestBody RequestNumber requestNum) {
+        return String.valueOf(requestNum.getNumber1() + requestNum.getNumber2());
     }
 }
